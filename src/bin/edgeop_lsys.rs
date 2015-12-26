8d3276aaadbbb0914871c49b79c1453388542de1
Michael Neumann <mneumann@ntecs.de>
2015-12-26 12:08:15 +0100

    Rename arity to symbol_arity which is more self-explanatory

--- src/bin/edgeop_lsys.rs
+++ src/bin/edgeop_lsys.rs
@@ -51,7 +51,7 @@ struct ConfigGenome {
     max_iter: usize,
     rules: usize,
     initial_len: usize,
-    arity: usize,
+    symbol_arity: usize,
     prob_terminal: Probability
 }
 
@@ -169,7 +169,7 @@ fn parse_config(sexp: Sexp) -> Config {
         var_ops: var_ops,
         genome: ConfigGenome {
             rules: genome_map.get("rules").and_then(|v| v.get_uint()).unwrap() as usize,
-            arity: genome_map.get("arity").and_then(|v| v.get_uint()).unwrap() as usize,
+            symbol_arity: genome_map.get("symbol_arity").and_then(|v| v.get_uint()).unwrap() as usize,
             initial_len: genome_map.get("initial_len").and_then(|v| v.get_uint()).unwrap() as usize,
             max_iter: genome_map.get("max_iter").and_then(|v| v.get_uint()).unwrap() as usize,
             prob_terminal: Probability::new(genome_map.get("prob_terminal").and_then(|v| v.get_float()).unwrap() as f32),
@@ -209,7 +209,7 @@ fn main() {
                                    config.genome.max_iter, // iterations
                                    config.genome.rules, // num_rules
                                    config.genome.initial_len, // initial rule length
-                                   config.genome.arity, // we use 2-ary symbols
+                                   config.genome.symbol_arity, // we use 2-ary symbols
                                    config.genome.prob_terminal,
                                    2, // max_expr_depth
 
